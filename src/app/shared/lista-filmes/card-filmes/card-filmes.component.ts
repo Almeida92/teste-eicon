c62b134f8d24f4402bb6463d4916a5e58bb248a9
Felipe Almeida <felipe.almeida@jsl.com.br>
2019-11-12 19:51:06 -0200

    integrando lista

--- src/app/shared/lista-filmes/card-filmes/card-filmes.component.ts
+++ src/app/shared/lista-filmes/card-filmes/card-filmes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import Filme from '../../models/filme.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-card-filmes',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-filmes.component.scss']
 })
 export class CardFilmesComponent implements OnInit {
+  @Input() filme: Filme;
+  URL_IMG: string = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/sYMAc4BLMHVAXmt7VSEnDM97Sy1.jpg';
 
   constructor() { }
 
   ngOnInit() {
+    this.URL_IMG = `${environment.URL_IMG}/${this.filme.poster_path}` ;
   }
 
 }
